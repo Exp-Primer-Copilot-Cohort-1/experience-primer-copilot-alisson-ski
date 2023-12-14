486ef357b3a77e931930df949a85a5dfcd3bb971
Alisson Lucas Lewinski <149223866+alisson-ski@users.noreply.github.com>
2023-12-14 19:31:15 +0000

    Copilot third commit v3

--- comment.js
+++ comment.js
@@ -1,33 +1,16 @@
-// create web server
-// run server: node comment.js
-// open browser: http://localhost:8080
-// press ctrl+c to stop server
-//==============================================
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-var http = require('http');
-var url = require('url');
+// Use body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 
-var server = http.createServer(function (request, response) {
-    var path = url.parse(request.url).pathname;
-    console.log(path);
-    switch (path) {
-        case '/':
-            response.writeHead(200, { 'Content-Type': 'text/plain' });
-            response.write('Hello World');
-            response.end();
-            break;
-        case '/about':
-            response.writeHead(200, { 'Content-Type': 'text/plain' });
-            response.write('About');
-            response.end();
-            break;
-        default:
-            response.writeHead(404);
-            response.write("opps this doesn't exist - 404");
-            response.end();
-            break;
-    }
+// Use ejs
+app.set('view engine', 'ejs');
+
+// Create server
+app.listen(3000, function() {
+    console.log('Server running at http://localhost:3000')
 });
-
-server.listen(8080);
-console.log('Server is running at http://localhost:8080');
