03ede43c694271a3f3dd7834bc35ab290b9a1292
Alisson Lucas Lewinski <149223866+alisson-ski@users.noreply.github.com>
2023-12-14 18:32:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,93 @@
+function skillsMember() {
+  var skills = [
+    {
+      name: "HTML",
+      icon: "fab fa-html5",
+      color: "#e34c26",
+      level: 90,
+    },
+    {
+      name: "CSS",
+      icon: "fab fa-css3-alt",
+      color: "#264de4",
+      level: 90,
+    },
+    {
+      name: "JavaScript",
+      icon: "fab fa-js-square",
+      color: "#f0db4f",
+      level: 80,
+    },
+    {
+      name: "React",
+      icon: "fab fa-react",
+      color: "#61dbfb",
+      level: 80,
+    },
+    {
+      name: "Vue",
+      icon: "fab fa-vuejs",
+      color: "#41b883",
+      level: 70,
+    },
+    {
+      name: "Node",
+      icon: "fab fa-node-js",
+      color: "#3c873a",
+      level: 70,
+    },
+    {
+      name: "PHP",
+      icon: "fab fa-php",
+      color: "#8892be",
+      level: 60,
+    },
+    {
+      name: "Python",
+      icon: "fab fa-python",
+      color: "#3776ab",
+      level: 60,
+    },
+    {
+      name: "MySQL",
+      icon: "fas fa-database",
+      color: "#4479a1",
+      level: 60,
+    },
+    {
+      name: "MongoDB",
+      icon: "fas fa-database",
+      color: "#4db33d",
+      level: 60,
+    },
+    {
+      name: "Git",
+      icon: "fab fa-git-alt",
+      color: "#f34f29",
+      level: 70,
+    },
+    {
+      name: "AWS",
+      icon: "fab fa-aws",
+      color: "#ff9900",
+      level: 60,
+    },
+    {
+      name: "Docker",
+      icon: "fab fa-docker",
+      color: "#2496ed",
+      level: 60,
+    },
+    {
+      name: "Linux",
+      icon: "fab fa-linux",
+      color: "#f1c40f",
+      level: 70,
+    },
+    {
+      name: "WordPress",
+      icon: "fab fa-wordpress",
+      color: "#217"
+    }
+  ]
+};
