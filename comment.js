2d4cb5a9064d3ff6dab4cd27d6d125afef3222d3
Alisson Lucas Lewinski <149223866+alisson-ski@users.noreply.github.com>
2023-12-14 19:25:38 +0000

    Copilot third commit v2

--- comment.js
+++ comment.js
@@ -1,31 +1,33 @@
-//Create web server
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-const auth = require('../middleware/auth');
+// create web server
+// run server: node comment.js
+// open browser: http://localhost:8080
+// press ctrl+c to stop server
+//==============================================
 
-// Create comment
-// api/comment
-router.post('/',
-    auth,
-    commentController.createComment
-);
+var http = require('http');
+var url = require('url');
 
-// Get all comments
-router.get('/',
-    commentController.getComments
-);
+var server = http.createServer(function (request, response) {
+    var path = url.parse(request.url).pathname;
+    console.log(path);
+    switch (path) {
+        case '/':
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write('Hello World');
+            response.end();
+            break;
+        case '/about':
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write('About');
+            response.end();
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
+});
 
-// Update comment via ID
-router.put('/:id',
-    auth,
-    commentController.updateComment
-);
-
-// Delete comment via ID
-router.delete('/:id',
-    auth,
-    commentController.deleteComment
-);
-
-module.exports = router;
+server.listen(8080);
+console.log('Server is running at http://localhost:8080');
